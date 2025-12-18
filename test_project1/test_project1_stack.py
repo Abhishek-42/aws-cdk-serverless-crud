9c50907c844547f065cc4203c682285f90d05655
Abhishek-42 <abhishek2005abhi@gmail.com>
2025-12-18 20:47:20 +0530

    first commit

--- test_project1/test_project1_stack.py
+++ test_project1/test_project1_stack.py
@@ -1,8 +1,13 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_dynamodb as dynamodb,
+    aws_lambda as lambda_,
+    aws_apigateway as apigw,
+    aws_iam as iam
+
 )
+import os
 from constructs import Construct
 
 class TestProject1Stack(Stack):
@@ -10,10 +15,41 @@ class TestProject1Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        table = dynamodb.Table(
+            self,
+            "TasksTable",
+            partition_key=dynamodb.Attribute(
+                name="task_id",
+                type=dynamodb.AttributeType.STRING
+            )
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TestProject1Queue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        task_lambda = lambda_.Function(
+            self,
+            "TasksHandler",
+            runtime=lambda_.Runtime.PYTHON_3_10,
+            handler="handler.main",
+            code=lambda_.Code.from_asset("lambda")
+        )
+
+        api = apigw.RestApi(
+            self,
+            "TasksApi",
+            rest_api_name="Tasks Service"
+        )
+        tasks = api.root.add_resource("tasks")
+        tasks.add_method("GET", apigw.LambdaIntegration(task_lambda))
+        tasks.add_method("POST", apigw.LambdaIntegration(task_lambda))
+
+        task = tasks.add_resource("{id}")
+        task.add_method("PUT", apigw.LambdaIntegration(task_lambda))
+        task.add_method("DELETE", apigw.LambdaIntegration(task_lambda))
+
+        table.grant_read_write_data(task_lambda)
+
+        task_lambda.add_environment(
+            "TABLE_NAME",
+            table.table_name
+        )   
+
+        table_name = os.environ["TABLE_NAME"]
